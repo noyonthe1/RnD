f79a15208259e55ad0dfd7c79003ba0c4f0fda4f
Rezwan <rezwan_17@yahoo.com>
2017-07-18 18:57:11 +0600

    checked in after ca3 added

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -158,6 +158,10 @@ function replacePrivateKey () {
   PRIV_KEY=$(ls *_sk)
   cd $CURRENT_DIR
   sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+  cd crypto-config/peerOrganizations/org3.example.com/ca/
+  PRIV_KEY=$(ls *_sk)
+  cd $CURRENT_DIR
+  sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
   # If MacOSX, remove the temporary backup of the docker-compose file
   if [ "$ARCH" == "Darwin" ]; then
     rm docker-compose-e2e.yamlt
